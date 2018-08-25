40928d194b0fd6cdf1ce89f2a4163a0392b3c1fe
syzer3@gmail.com <syzer3@gmail.com>
2018-08-25 21:35:19 +0200

    Frontend: make angular work

--- frontend/angular/src/app/new-damage/new-damage.component.ts
+++ frontend/angular/src/app/new-damage/new-damage.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-new-damage',
   templateUrl: './new-damage.component.html',
   styleUrls: ['./new-damage.component.css']
 })
-export class NewDamageComponent implements OnInit {
 
-  constructor() { }
+export class NewDamageComponent implements OnInit {
+  @Input()
+  url: string = "http://127.0.0.1:4000/";
+  urlSafe: SafeResourceUrl;
+
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
-
 }
