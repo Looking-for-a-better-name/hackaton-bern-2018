a327bf7ab9220a32e74ca536810df4d1a4879c80
Nicole <ni.widmer@bluewin.ch>
2018-08-25 17:49:02 +0200

    Router added for Schadensfall aufnehmen

---
+++ frontend/angular/src/app/new-damage/new-damage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-damage',
+  templateUrl: './new-damage.component.html',
+  styleUrls: ['./new-damage.component.css']
+})
+export class NewDamageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
