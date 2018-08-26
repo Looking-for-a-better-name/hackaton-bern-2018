c602c5f7cfa55362bcd9da292a60c204a9820a37
syzer3@gmail.com <syzer3@gmail.com>
2018-08-26 10:14:40 +0200

    Add escape for pandas parser

--- backend/jsonToCsv.js
+++ backend/jsonToCsv.js
@@ -3,8 +3,10 @@ const fs  = require('fs')
 const ost  = require('object-stream-tools')
 const db = require('../data/dbEn')
 
+const esc  = str => (str || '').replace(`"`, `'`)
+
 const csv = db.map(e =>
-  e.dmg + `;` + e.desc + `;` + e.cat)
-csv.unshift('dmg;desc;cat')
+  `"${esc(e.dmg)}","${esc(e.desc)}","${e.cat}"`)
+csv.unshift(`"dmg","desc","cat"`)
 
 fs.writeFileSync(__dirname + '/../data/dbEn.csv', csv.join('\n'), 'UTF-8')
