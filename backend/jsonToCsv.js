a727912d262584e49d2ba8c935512d435d6e8875
syzer3@gmail.com <syzer3@gmail.com>
2018-08-26 14:40:41 +0200

    WARNING! This will require node server to accept string as label. AKA TODO labels instead number after presentation for python flask swrver

--- backend/jsonToCsv.js
+++ backend/jsonToCsv.js
@@ -5,8 +5,16 @@ const db = require('../data/dbEn')
 
 const esc  = str => (str || '').replace(`"`, `'`)
 
+const toClaimType = label => ({
+  0: 'electrical',
+  1: 'water',
+  2: 'vandalism',
+  3: 'other',
+  4: 'insects'
+}[label] || 'other')
+
 const csv = db.map(e =>
-  `"${esc(e.dmg)}","${esc(e.desc)}","${e.cat}"`)
+  `"${esc(e.dmg)}","${esc(e.desc)}","${toClaimType(e.cat)}"`)
 csv.unshift(`"dmg","desc","cat"`)
 
 fs.writeFileSync(__dirname + '/../data/dbEn.csv', csv.join('\n'), 'UTF-8')
